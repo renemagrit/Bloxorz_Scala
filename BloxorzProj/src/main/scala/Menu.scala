06a3c6218babb6c142dd87d4812ffec35517cc41
renemagrit <41620470+renemagrit@users.noreply.github.com>
2022-05-28 15:29:52 +0200

    Map loading added

--- BloxorzProj/src/main/scala/Menu.scala
+++ BloxorzProj/src/main/scala/Menu.scala
@@ -1,3 +1,4 @@
+import scala.io.Source
 import scala.io.StdIn.readLine
 
 class Menu {
@@ -12,7 +13,20 @@ class Menu {
     println("4. Odigraj sekvencu poteza iz fajla")
     println("Unos: ")
 
-    val command = readLine()
+    val command = readLine.toInt
+
     println("Izabrai ste komandu " + command)
+
+    command match {
+      case 1 => loadMapFromFile()
+      case _ => println("Ponovite unos")
+    }
+  }
+
+  def loadMapFromFile()={
+    val filename = "D:\\Marta SI\\Master\\FP\\Bloxorz_Scala\\BloxorzProj\\src\\main\\maps\\map1.txt"
+    for (line <- Source.fromFile(filename).getLines) {
+      println(line)
+    }
   }
 }
