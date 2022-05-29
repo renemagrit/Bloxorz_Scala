f1a5f62676b2edb1a4483bf7fef34c193df75c20
renemagrit <41620470+renemagrit@users.noreply.github.com>
2022-05-29 16:02:08 +0200

    Load map: Check if file exists added

--- BloxorzProj/src/main/scala/Menu.scala
+++ BloxorzProj/src/main/scala/Menu.scala
@@ -1,3 +1,7 @@
+
+import java.nio.file.Files
+import java.nio.file.Paths
+
 import scala.io.Source
 import scala.io.StdIn.readLine
 
@@ -5,6 +9,7 @@ class Menu {
 
   val myMaps = new Maps()
 
+  //@tailrec
   def showMenu():Unit={
     println("- - - - - - - -  MENU - - - - - - - - ")
     println("Izaberite jednu od ponudjenih opcija:")
@@ -28,19 +33,24 @@ class Menu {
 
 
   def readMapFromFile()={
-    //TODO: add option to insert path for new file
     println("Unesite ime fajla: ")
     val fileName = readLine.toString
 
     // Path is alerady defined with the folder maps
     val filePath = "D:\\Marta SI\\Master\\FP\\Bloxorz_Scala\\BloxorzProj\\src\\main\\maps\\"+fileName+".txt"
 
-    //Reade file and load into strList variable
-    var strList = ""
-    for (line <- Source.fromFile(filePath).getLines) strList = strList + line.mkString + "\n"
+    //Check if file exists
+    if (!Files.exists(Paths.get(filePath))){
+        println("Uneto ime fajla ne postoji!")
 
+    }else {
+      //Reade file and load into strList variable
+      var strList = ""
+      for (line <- Source.fromFile(filePath).getLines) strList = strList + line.mkString + "\n"
 
-    myMaps.addMap(fileName, strList.toString() )
-    myMaps.listOfAvailMaps()
+      //Add loaded map to the list of available maps
+      myMaps.addMap(fileName, strList.toString())
+      myMaps.listOfAvailMaps()
+    }
   }
 }
